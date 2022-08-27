2230cbaad9f26a578d47f1a5d9e39278bb360d49
SkapeBreak <oriskatebreakginal@gmail.com>
2022-08-27 14:46:39 -0600

    Routes missing, added

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
-
+use App\http\controllers\MessagesController;
+use App\http\controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,6 +14,27 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+// Route::resource('messages', MessagesController::class);
+
+// Public routes
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+// Protected routes
+
+Route::group(['middleware' => ['auth:sanctum']], function(){
+    Route::get('/messages/search/{message}', [MessagesController::class, 'search']);
+    Route::get('/messages', [MessagesController::class, 'index']);
+    Route::get('/messages/{id}', [MessagesController::class, 'show']);
+    Route::post('/messages', [MessagesController::class, 'store']);
+    Route::put('/messages/{id}', [MessagesController::class, 'update']);
+    Route::delete('/messages/{id}', [MessagesController::class, 'destroy']);
+    Route::post('/logout', [AuthController::class, 'logout']);    
+});
+
+
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
