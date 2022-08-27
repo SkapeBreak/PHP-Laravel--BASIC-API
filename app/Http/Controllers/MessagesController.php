bcd021212e8c4f216cb74e7b47d93dbd2f24708f
SkapeBreak <oriskatebreakginal@gmail.com>
2022-08-27 14:36:02 -0600

    basic CRUD added

---
+++ app/Http/Controllers/MessagesController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Messages;
+use Illuminate\Http\Request;
+
+class MessagesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Messages::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'message' => 'required'
+        ]);
+
+        return Messages::create($request->all());
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return Messages::find($id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $message = Messages::find($id);
+        $message->update($request->all());
+        return $message;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        return Messages::destroy($id);
+    }
+
+    /**
+     * search for a name.
+     *
+     * @param  int  $message
+     * @return \Illuminate\Http\Response
+     */
+    public function search($message)
+    {
+        return Messages::where('message', 'like', '%'.$message.'%')->get();
+    }
+}
